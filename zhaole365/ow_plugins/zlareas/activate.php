b70c93d2ce5823b62e2298498c27eceea9ba8995
Hawk Wang <wanghao.buaa@gmail.com>
2014-11-04 23:15:06 +0800

    update with translated for virtual gifts plugin

--- zhaole365/ow_plugins/zlareas/activate.php
+++ zhaole365/ow_plugins/zlareas/activate.php
@@ -8,3 +8,7 @@ $navigation->addMenuItem(
 		'zlareas',
 		'main_menu_list',
 		OW_Navigation::VISIBLE_FOR_ALL);
+
+// hacking - 删除下面菜单的广告项 ：－）
+OW::getNavigation()->deleteMenuItem('base', 'openwack');
+OW::getNavigation()->deleteMenuItem('base', 'wackwall');
