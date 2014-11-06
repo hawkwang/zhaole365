79369b301aa980e51f89b9b5cce3148905e2efb2
Hawk Wang <wanghao.buaa@gmail.com>
2014-11-06 16:25:21 +0800

    update for ignore

--- zhaole365/ow_plugins/zlareas/bol/location_service.php
+++ zhaole365/ow_plugins/zlareas/bol/location_service.php
@@ -14,37 +14,22 @@
  * @since 1.0
  */
 
-final class LOCATIONTAG_BOL_LocationService
+final class ZLAREAS_BOL_LocationService
 {
     const JQUERY_LOAD_PRIORITY = 100000002;
     
     const SESSION_VAR_ENTITY_LIST = 'googlelocation_userlist_session_var';
     const ENTITY_TYPE_STATUS_UPDATE = 'user-status';
 
-    /**
-     * @var LOCATIONTAG_BOL_LocationDao
-     */
+
     private $locationDao;
-    /**
-     * Class instance
-     *
-     * @var LOCATIONTAG_BOL_LocationService
-     */
     private static $classInstance;
 
-    /**
-     * Class constructor
-     */
     private function __construct()
     {
-        $this->locationDao = LOCATIONTAG_BOL_LocationDao::getInstance();
+        $this->locationDao = ZLAREAS_BOL_LocationDao::getInstance();
     }
 
-    /**
-     * Returns class instance
-     *
-     * @return LOCATIONTAG_BOL_LocationService
-     */
     public static function getInstance()
     {
         if ( self::$classInstance === null )
@@ -55,85 +40,43 @@ final class LOCATIONTAG_BOL_LocationService
         return self::$classInstance;
     }
 
-    public function save( LOCATIONTAG_BOL_Location $dto )
+    public function save( ZLAREAS_BOL_Location $dto )
     {
         $this->locationDao->save($dto);
     }
 
-    public function addLocation( $entityId, $entityType, $address, $json )
+    public function addLocation( $address, $longitude, $latitude, $areacode, $description )
     {
-        if ( empty($json) || empty($entityType) || empty($entityId) || empty($address) )
+        if ( empty($latitude) || empty($longitude) || empty($areacode) || empty($address)  || empty($description) )
         {
             return;
         }
 
-        $data = json_decode($json, true);
-
-        $country = '';
-
-        foreach ( $data['result']['address_components'] as $item )
-        {
-            if ( !empty($item['types']) )
-            {
-                foreach ( $item['types'] as $type )
-                {
-                    if ( $type == 'country' )
-                    {
-                        $country = !empty($item['short_name']) ? $item['short_name'] : '';
-                        break;
-                    }
-                }
-            }
-
-            if ( !empty($country) )
-            {
-                break;
-            }
-        }
-        
-        $dto = new LOCATIONTAG_BOL_Location();
-        $dto->entityId = (int)$entityId;
-        $dto->entityType = $entityType;
-        $dto->countryCode = $country;
+        $dto = new ZLAREAS_BOL_Location();
         $dto->address = $address;
-        $dto->lat = !empty($data['lat']) ? $data['lat'] : 0;
-        $dto->lng = !empty($data['lng']) ? $data['lng'] : 0;
-        $dto->southWestLat = !empty($data['southWestLat']) ? $data['southWestLat'] : 0;
-        $dto->southWestLng = !empty($data['southWestLng']) ? $data['southWestLng'] : 0;
-        $dto->northEastLat = !empty($data['northEastLat']) ? $data['northEastLat'] : 0;
-        $dto->northEastLng = !empty($data['northEastLng']) ? $data['northEastLng'] : 0;
-        $dto->json = $json;
+        $dto->longitude = $longitude;
+        $dto->latitude = $latitude;
+        $dto->areacode = $areacode;
+        $dto->address = $address;
+        $dto->description = $description;
         
         $this->save($dto);
     }
-    public function getAllLocationsForEntityType( $entityType )
+    
+    public function getAllLocations( )
     {
-        return $this->locationDao->getAllLocationsForEntityType($entityType);
+        return $this->locationDao->getAllLocations();
+    }
+    
+    public function findLocationByAddress( $address )
+    {
+    	return $this->locationDao->findLocationByAddress( $address );
     }
 
-    public function getLanguageCode()
+    public function deleteById( $id )
     {
-        $tag = BOL_LanguageService::getInstance()->getCurrent()->getTag();
-        $matches = array();
-        preg_match("/^([a-zA-Z]{2})-[a-zA-Z]{2}.*$/", $tag, $matches);
-        $language = 'en';
-
-        if ( !empty($matches[1]) )
-        {
-            $language = mb_strtolower($matches[1]);
-        }
-
-        return $language;
-    }
-
-    public function findByEntityIdAndEntityType( $entityId, $entityType )
-    {
-        return $this->locationDao->findEntityIdAndEntityType($entityId, $entityType);
-    }
-
-    public function deleteByEntityIdAndEntityType( $entityId, $entityType )
-    {
-        return $this->locationDao->deleteByEntityIdAndEntityType($entityId, $entityType);
+    	return $this->locationDao->deleteByIdList( array($id) );
+        //return $this->locationDao->deleteById( $id );
     }
     
     public function findById( $id )
